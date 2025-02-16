723c6fa530ee09f7246c8664ed5370f0ef7cd9a3
Hoai Nam <nvhnam.work@gmail.com>
2025-02-16 23:19:57 +0700

    feat: update blog, add-blog UI

---
+++ src/app/shared/components/blog-loading/blog-loading.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-loading',
+  imports: [],
+  templateUrl: './blog-loading.component.html',
+  styleUrl: './blog-loading.component.css'
+})
+export class BlogLoadingComponent {
+
+}
