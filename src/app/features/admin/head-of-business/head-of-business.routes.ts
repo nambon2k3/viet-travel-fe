5101962d5ad300ed5ca009e7a1f710d7c98cf5b0
PHAM HUONG LY <huongly2003.hn@gmail.com>
2025-03-04 22:31:53 +0700

    merge iter1

--- src/app/features/admin/head-of-business/head-of-business.routes.ts
+++ src/app/features/admin/head-of-business/head-of-business.routes.ts
@@ -2,21 +2,13 @@ import { Route } from "@angular/router";
 import { LayoutComponent } from "../layout/layout.component";
 import { ListLocationComponent } from "./components/list-location/list-location.component";
 import { PostLocationDetailComponent } from "./components/post-location-detail/post-location-detail.component";
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-import { ViewListTourComponent } from "./components/view-list-tour/view-list-tour.component";
 import { AddTourComponent } from "./components/add-tour/add-tour.component";
 import { TourDayComponent } from "./components/tour-details/tour-day/tour-day.component";
 import { TourDetailsLayoutComponent } from "./components/tour-details/tour-details-layout/tour-details-layout.component";
 import { TourDetailsComponent } from "./components/tour-details/tour-details.component";
 import { TourPaxComponent } from "./components/tour-details/tour-pax/tour-pax.component";
 import { UpdateTourDayComponent } from "./components/tour-details/update-tour-day/update-tour-day.component";
->>>>>>> Stashed changes
-=======
 import { ViewListTourComponent } from "./components/view-list-tour/view-list-tour.component";
-import { AddTourComponent } from "./components/add-tour/add-tour.component";
->>>>>>> iter1
 
 export const HEAD_OF_BUSINESS_ROUTES: Route[] = [
   {
@@ -36,11 +28,6 @@ export const HEAD_OF_BUSINESS_ROUTES: Route[] = [
         path: 'location-details',
         component: PostLocationDetailComponent,
       },
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> iter1
       {
         path: 'list-tour',
         component: ViewListTourComponent,
@@ -49,7 +36,6 @@ export const HEAD_OF_BUSINESS_ROUTES: Route[] = [
         path: 'add-tour',
         component: AddTourComponent,
       },
-<<<<<<< HEAD
       {
         path: '',
         component: TourDetailsLayoutComponent,
@@ -61,9 +47,6 @@ export const HEAD_OF_BUSINESS_ROUTES: Route[] = [
           { path: '', redirectTo: 'details', pathMatch: 'full' },
         ],
       },
->>>>>>> Stashed changes
-=======
->>>>>>> iter1
     ]
   },
 ];
