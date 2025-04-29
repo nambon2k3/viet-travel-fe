8137850382405ff19b0a52d324dca87cfb19e952
PHAM HUONG LY <huongly2003.hn@gmail.com>
2025-04-30 00:08:49 +0700

    refactor: summary

--- src/app/features/admin/operator/components/tour-operation/summary/summary.component.ts
+++ src/app/features/admin/operator/components/tour-operation/summary/summary.component.ts
@@ -77,8 +77,8 @@ export class SummaryComponent implements OnInit {
   
           const profit = parseFloat(tourData.actualProfitAmount);
           this.summary = [
-            { content: 'Đã thu', estimate: parseFloat(tourData.estimateReceiptAmount), summary: parseFloat(tourData.receiptedAmount) },
-            { content: 'Đã chi', estimate: parseFloat(tourData.estimatedPaymentAmount), summary: parseFloat(tourData.paymentAmount) },
+            { content: 'Đã thu', estimate: parseFloat(tourData.estimateReceiptAmount), summary: parseFloat(tourData.totalReceiptAmount) },
+            { content: 'Đã chi', estimate: parseFloat(tourData.estimatedPaymentAmount), summary: parseFloat(tourData.totalPaymentAmount) },
             { content: 'Lợi nhuận', estimate: parseFloat(tourData.estimateProfitAmount), summary: profit }
           ];
         }
